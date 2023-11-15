0a517e1dbb456fb569b611d6902cc6318cc07fde
LucasNFortes <lucas.fortes5052@gmail.com>
2023-11-15 19:07:01 -0400

    commit

---
+++ scripts/login.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const formItems = document.querySelectorAll('.form-item');
+
+    formItems.forEach(function (formItem) {
+        const input = formItem.querySelector('.form-style');
+
+        input.addEventListener('input', function () {
+            if (input.value.trim() !== '') {
+                formItem.classList.add('input-focus');
+            } else {
+                formItem.classList.remove('input-focus');
+            }
+        });
+    });
+});
