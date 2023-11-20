40c11745db29737e6584c166d718c2fb80bde3e6
LucasNFortes <lucas.fortes5052@gmail.com>
2023-11-20 08:38:45 -0400

    include css js

--- app.js
+++ app.js
@@ -7,3 +7,18 @@ app.get('/', (req, res) => {
 });
 
 app.listen(3000);
+
+//----------------------------------------------------
+
+const glob = require('glob');
+
+const cssFiles = glob.sync('**/css/*.css');
+const jsFiles = glob.sync('**/scripts/*.js');
+
+for (const file of cssFiles) {
+  require(file);
+}
+
+for (const file of jsFiles) {
+  require(file);
+}
